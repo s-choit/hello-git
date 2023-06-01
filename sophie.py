d3c1fefa89faa41d7e91b3b2f151223c08407218
sophiiecheng <75765535+sophiiecheng@users.noreply.github.com>
2023-06-01 10:25:32 -0700

    added code for saving & loading game

--- sophie.py
+++ sophie.py
@@ -1,1 +1,12 @@
 #print("Hello world")
+def save_game(board):
+    with open('tic_tac_toe.json','w') as file:
+        json.dump(board,file)
+    print("Game successfuly ")
+
+def load_game():
+    try:
+        with open('tic_tac_toe.json','r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return None
