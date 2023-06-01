17afa5e25985f14f9459381bf7b1d4d751a82128
sophiiecheng <75765535+sophiiecheng@users.noreply.github.com>
2023-06-01 10:38:11 -0700

    code for saving & loading game

--- group.py
+++ group.py
@@ -71,3 +71,14 @@ def is_winner (board, mark):
         return True
     return False
 
+def save_game(board):
+    with open('tic_tac_toe.json','w') as file:
+        json.dump(board, file)
+    print("Game Successfully")
+
+def load_game():
+    try:
+        with open('tic_tac_toe.json','r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return None
